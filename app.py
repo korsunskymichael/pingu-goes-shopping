d75df077920e222efd25a55788cc243ed9072ee6
Michael Korsunsky <korsunskymichael@gmail.com>
2023-05-08 23:02:25 +0300

    commit

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from static.python.functions import *
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pingu.db'
 
 buyer_id = '12358'
 
